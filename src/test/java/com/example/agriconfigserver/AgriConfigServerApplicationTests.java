9ddc55b03d64ac1838450b203170d91c30b6a400
felixmaina <karani.maina2010@gmail.com>
2022-10-28 11:21:24 +0300

    first commit

---
+++ src/test/java/com/example/agriconfigserver/AgriConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agriconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
